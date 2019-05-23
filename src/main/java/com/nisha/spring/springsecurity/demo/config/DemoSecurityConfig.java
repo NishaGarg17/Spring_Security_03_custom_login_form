3a23b28ab400bd1c59f20bed8dabffe81df89874
Nisha Garg <14gargnisha@gmail.com>
2019-05-23 08:22:44 +0530

    Custom Login form

--- src/main/java/com/nisha/spring/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/nisha/spring/springsecurity/demo/config/DemoSecurityConfig.java
@@ -17,9 +17,14 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 		// add our users for in memory authentication
 		UserBuilder users = User.withDefaultPasswordEncoder();
 		auth.inMemoryAuthentication().withUser(users.username("John").password("John").roles("EMPLOYEE"))
-		.withUser(users.username("Mary").password("John").roles("MANAGER"))
-		.withUser(users.username("Susan").password("John").roles("ADMIN"));
+				.withUser(users.username("Mary").password("John").roles("MANAGER"))
+				.withUser(users.username("Susan").password("John").roles("ADMIN"));
 	}
-	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/showMyLoginPage").loginProcessingUrl("/authenticateTheUser")
+				.permitAll();
+	}
 
 }
